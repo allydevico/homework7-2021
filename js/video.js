4d0ccd0f5279c2723b2b5fac797fa3561e634044
allydevico <71455118+allydevico@users.noreply.github.com>
2021-11-18 18:01:26 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
@@ -13,5 +13,11 @@ document.querySelector("#play").addEventListener("click", function() {
  document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
 	video.pause();
+ });
+
+ document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate = video.playbackRate - 0.05;
+	video.play();
  });
 
